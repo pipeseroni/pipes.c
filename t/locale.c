c966f7106dc53d84c473aa64c166629b7cebd1f8
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2018-04-11 18:06:11 +0100

    Fix locale unit test
    The previous test always returned an exit status of zero, and the test
    driver wasn't actually interpreting the TAP output so the test always
    passed. This commit removes the attempt at parsing the TAP output, and
    makes sure to set the exit value correctly upon test failure.

--- t/locale.c
+++ t/locale.c
@@ -5,16 +5,21 @@
 #include <string.h>
 
 #include "pipe.h"
+#include "err.h"
 /**
  * Check locale-encoding functions.
  */
-#define NUM_TESTS 29
+#define NUM_TESTS 30
 
 // TAP-producing ok() function
-static int testnum = 0;
+static int testnum = 1;
+static int failures = 0;
+
 static void ok(bool condition, const char* msg);
 static void ok(bool condition, const char* msg) {
     printf("%s %d - %s\n", condition ? "ok" : "not ok", testnum++, msg);
+    if(!condition)
+        failures++;
 }
 
 // These are the same in UTF8 and GB18030
@@ -28,7 +33,7 @@ const char *GB18030_BOX = "\xA9\xA5\xA9\xA7";
 const char *GB18030_BOX_SPLIT = "\xA9\xA5\0\xA9\xA7";
 
 // Assigned to by assign_matrices
-const char *test_pipe_chars = "1\0" "2\0" "3\0" "4\0" "5\0" "6";
+const char test_pipe_chars[] = "1\0" "2\0" "3\0" "4\0" "5\0" "6";
 char *transition[16];
 char *continuation[2];
 
@@ -48,10 +53,17 @@ int main(int argc, char **argv) {
         "ASCII UTF8 to UTF8 in 4 bytes: no error code.");
     ok(strcmp(out, UTF8_ASCII) == 0,
         "ASCII UTF8 to UTF8 copied correctly");
+    clear_error();
 
     r = locale_to_utf8(in, out, "UTF-8", 3);
-    ok(r == -1,
+    ok(r == ERR_BUFFER_TOO_SMALL,
         "ASCII UTF8 to UTF8 in 3 bytes: buffer too small.");
+
+    ok(strstr(string_error(), "Buffer (3 items) too small"),
+        "Error text contains buffer size.");
+    ok(strstr(string_error(), "tried to insert 4 items"),
+        "Error text contains inserted size.");
+    clear_error();
 
     // Identity conversion with higher bytes
     strcpy(in, UTF8_BOX);
@@ -60,10 +72,12 @@ int main(int argc, char **argv) {
         "Box UTF8 to UTF8 in 7 bytes: no error code.");
     ok(strcmp(out, UTF8_BOX) == 0,
         "Box UTF8 to UTF8 copied correctly.");
+    clear_error();
 
     r = locale_to_utf8(in, out, "UTF-8", 6);
-    ok(r == -1,
+    ok(r == ERR_BUFFER_TOO_SMALL,
         "Box UTF8 to UTF8 in 6 bytes: buffer too small.");
+    clear_error();
 
     // Convert GB18030 to UTF-8 with ASCII bytes (no change)
     strcpy(in, GB18030_ASCII);
@@ -72,10 +86,12 @@ int main(int argc, char **argv) {
         "ASCII GB18030 to UTF8 in 4 bytes: no error code.");
     ok(strcmp(out, UTF8_ASCII) == 0,
         "ASCII UTF8 to GB18030 copied correctly");
+    clear_error();
 
     r = locale_to_utf8(in, out, "GB18030", 3);
-    ok(r == -1,
+    ok(r == ERR_ICONV_ERROR,
         "ASCII GB18030 to UTF8 in 3 bytes: buffer too small.");
+    clear_error();
 
     // Convert GB18030 to UTF-8 with box-drawing chars
     strcpy(in, GB18030_BOX);
@@ -84,10 +100,12 @@ int main(int argc, char **argv) {
         "Box GB18030 to UTF8 in 7 bytes: no error code.");
     ok(strcmp(out, UTF8_BOX) == 0,
         "Box GB18030 to UTF8 copied correctly.");
+    clear_error();
 
     r = locale_to_utf8(in, out, "GB18030", 6);
-    ok(r == -1,
+    ok(r == ERR_ICONV_ERROR,
         "Box GB18030 to UTF8 in 6 bytes: buffer too small.");
+    clear_error();
 
     // Convert UTF-8 to GB18030 with ASCII bytes
     strcpy(in, UTF8_ASCII);
@@ -96,10 +114,12 @@ int main(int argc, char **argv) {
         "ASCII UTF8 to GB18030 in 6 bytes: no error code.");
     ok(memcmp(out, GB18030_ASCII_SPLIT, 6) == 0,
         "ASCII UTF8 to GB18030 copied correctly.");
+    clear_error();
 
     r = utf8_to_locale(in, out, 5, "GB18030");
-    ok(r == -1,
+    ok(r == ERR_BUFFER_TOO_SMALL,
         "ASCII UTF8 to GB18030 in 5 bytes: buffer too small.");
+    clear_error();
 
     // Convert UTF-8 to GB18030 with box-drawing bytes
     strcpy(in, UTF8_BOX);
@@ -108,15 +128,18 @@ int main(int argc, char **argv) {
         "Box UTF8 to GB18030 in 6 bytes: no error code.");
     ok(memcmp(out, GB18030_BOX_SPLIT, 6) == 0,
         "Box UTF8 to GB18030 copied correctly.");
+    clear_error();
 
     r = utf8_to_locale(in, out, 5, "GB18030");
-    ok(r == -1,
+    ok(r == ERR_ICONV_ERROR,
         "Box UTF8 to GB18030 in 5 bytes: buffer too small.");
+    clear_error();
 
     // Assign transition and continuation matrices
-    strcpy(in, test_pipe_chars);
-    r = assign_matrices(in, transition, continuation);
-    ok(r == 0, "No error assigning matrices.");
+    memcpy(in, test_pipe_chars, sizeof(test_pipe_chars));
+    in[sizeof(test_pipe_chars)] = '\0';
+
+    assign_matrices(in, transition, continuation);
 
     ok(!strcmp(continuation[0], "1"), "HORIZONTAL");
     ok(!strcmp(continuation[1], "2"), "VERTICAL");
@@ -132,5 +155,6 @@ int main(int argc, char **argv) {
 
     ok(!strcmp(transition[LEFT * 4 + DOWN], "6"), "LEFT / DOWN");
     ok(!strcmp(transition[UP * 4 + RIGHT], "6"), "UP / RIGHT");
-    return 0;
+    clear_error();
+    return (failures > 0) ? 1 : 0;
 }
