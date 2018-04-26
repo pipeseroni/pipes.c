e66af99415d93819035fc7c768fa3c238425d95d
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2018-04-26 22:55:11 +0100

    Implementation of a circular location buffer
    The location_buffer struct will be used to store a list of locations for
    a pipe in a circular buffer. When a pipe moves forwards, the buffer will
    be updated so that the previous tail is replaced by the next location,
    and the new head will be pushed.
    
    For a buffer of length 4:
    
            buffer     pushed
            1               1
            21              2
            321             3
            4321            4
            5432            5
            6543            6
    
    The implementation of this circular buffer is fairly standard. The
    buffer itself is a fixed-size portion of memory, and a head index is
    updated each time an element is pushed.

--- src/canvas.c
+++ src/canvas.c
@@ -109,3 +109,42 @@ void canvas_free(struct canvas *canvas) {
         pipe_cell_list_free(&canvas->cells[i]);
     free(canvas->cells);
 }
+
+cpipes_errno location_buffer_init(struct location_buffer *buffer, size_t max) {
+    buffer->locations = malloc(sizeof(*buffer->locations) * max);
+    if(!buffer->locations)
+        return set_error(ERR_OUT_OF_MEMORY);
+
+    buffer->max = max;
+    buffer->size = 0;
+    buffer->head = 0;
+    return 0;
+}
+void location_buffer_free(struct location_buffer *buffer) {
+    free(buffer->locations);
+}
+
+unsigned int *location_buffer_head(struct location_buffer *buffer) {
+    if(buffer->size == 0)
+        return NULL;
+    return &buffer->locations[buffer->head];
+}
+unsigned int *location_buffer_tail(struct location_buffer *buffer) {
+    if(buffer->size == 0)
+        return NULL;
+    size_t tail = (buffer->head + 1) % buffer->size;
+    return &buffer->locations[tail];
+}
+
+void location_buffer_push(struct location_buffer *buffer, unsigned int loc) {
+    if(buffer->size == 0) {
+        buffer->locations[buffer->head] = loc;
+        buffer->size++;
+    } else if(buffer->size < buffer->max) {
+        buffer->locations[++buffer->head] = loc;
+        buffer->size++;
+    } else {
+        buffer->head = (buffer->head + 1) % buffer->size;
+        buffer->locations[buffer->head] = loc;
+    }
+}
--- src/canvas.h
+++ src/canvas.h
@@ -51,5 +51,19 @@ cpipes_errno canvas_resize(
         unsigned int width, unsigned int height);
 void canvas_free(struct canvas *canvas);
 
+// Circular buffer containing the locations of a pipe
+struct location_buffer {
+    unsigned int *locations;
+    size_t max, size, head;
+};
+
+cpipes_errno location_buffer_init(struct location_buffer *buffer, size_t max);
+void location_buffer_free(struct location_buffer *buffer);
+
+unsigned int *location_buffer_head(struct location_buffer *buffer);
+unsigned int *location_buffer_tail(struct location_buffer *buffer);
+
+void location_buffer_push(struct location_buffer *buffer, unsigned int loc);
+
 #endif /* CANVAS_H_ */
 
