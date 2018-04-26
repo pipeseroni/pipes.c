639c7ab79bf33fd7a2bf9137a8b17cda0a1783d2
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2018-04-26 22:01:46 +0100

    Add linked list implentation for canvas cells
    The plan is to store the list of characters that have been drawn on each
    grid cell of the canvas in a linked list. Then, we can erase a character
    drawn by a particular pipe by looking backwards from the tail and
    popping the first matching pipe cell from the list.
    
    Then, if the list is empty we will erase that cell and if the list still
    contains elements, make sure that if the head of the linked list has
    changed, it is the character drawn to the terminal.
    
    For example, we might have the following list for a particular cell:
    
            pipe    char    color
            0x1     ━       1      ← Head
            0x2     ┃       2
            0x3     ━       1
            0x1     ┓       3      ← Tail
    
    If we remove pipe 0x1 from the list, the tail will point to 0x3, and the
    head is unchanged so we will not need to change the character drawn at
    this cell position. If we then remove 0x1 again, the head will point to
    0x2 and a "┃" character must be drawn with color 1. If we remove all
    pipes, that cell is erased.

---
+++ src/canvas.c
@@ -0,0 +1,65 @@
+#include <config.h>
+
+#include <stdlib.h>
+#include "pipe.h"
+#include "canvas.h"
+
+struct pipe_cell_list *pipe_cell_list_alloc(void) {
+    struct pipe_cell_list *p;
+    p = malloc(sizeof(*p));
+
+    pipe_cell_list_init(p);
+    return p;
+}
+
+void pipe_cell_list_init(struct pipe_cell_list *list) {
+    list->head = list->tail = NULL;
+}
+
+cpipes_errno pipe_cell_list_push(
+        struct pipe_cell_list *list,
+        const char *pipe_char, struct pipe *pipe) {
+
+    struct pipe_cell *cell;
+    cell = malloc(sizeof(*cell));
+    if(!cell)
+        return set_error(ERR_OUT_OF_MEMORY);
+
+    cell->pipe_char = pipe_char;
+    cell->color = pipe->color;
+    cell->pipe = pipe;
+
+    cell->next = list->head;
+    cell->prev = NULL;
+
+    if(!list->head)
+        list->tail = cell;
+    else
+        list->head->prev = cell;
+    list->head = cell;
+
+    return 0;
+}
+
+void pipe_cell_list_remove(struct pipe_cell_list *list, struct pipe *pipe) {
+    for(struct pipe_cell *c = list->tail; c; c = c->prev) {
+        if(c->pipe == pipe) {
+            if(c == list->head)
+                list->head = c->next;
+            if(c == list->tail)
+                list->tail = c->prev;
+            if(c->prev)
+                c->prev->next = c->next;
+            if(c->next)
+                c->next->prev = c->prev;
+            free(c);
+            break;
+        }
+    }
+}
+
+void pipe_cell_list_free(struct pipe_cell_list *list) {
+    for(struct pipe_cell *c = list->head; c; c = c->next)
+        free(c->prev);
+    free(list->tail);
+}
--- src/canvas.h
+++ src/canvas.h
@@ -5,6 +5,27 @@
 #include <stdint.h>
 #include "err.h"
 
+struct pipe;
+
+struct pipe_cell {
+    const char *pipe_char;
+    uint32_t color;
+    struct pipe *pipe;
+    struct pipe_cell *next, *prev;
+};
+
+struct pipe_cell_list {
+    struct pipe_cell *head, *tail;
+};
+
+struct pipe_cell_list *pipe_cell_list_alloc(void);
+void pipe_cell_list_init(struct pipe_cell_list *list);
+cpipes_errno pipe_cell_list_push(
+        struct pipe_cell_list *list,
+        const char *pipe_char, struct pipe *pipe);
+void pipe_cell_list_remove(struct pipe_cell_list *list, struct pipe *pipe);
+void pipe_cell_list_free(struct pipe_cell_list *list);
+
 struct palette {
     uint32_t *colors;
     size_t num_colors;
